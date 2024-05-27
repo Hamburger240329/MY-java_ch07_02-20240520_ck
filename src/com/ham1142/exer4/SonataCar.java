4c54e1ae1b5ee15534b7dcada01944c83b3542e8
JBEDU29 <5514cyj@gmail.com>
2024-05-27 09:28:49 +0900

    240527 0900 인터페이스 상속 복습

---
+++ src/com/ham1142/exer4/SonataCar.java
@@ -0,0 +1,29 @@
+package com.ham1142.exer4;
+
+public class SonataCar implements Car {
+
+	@Override
+	public void frontDrive() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void rearDrive() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopDrive() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void horn() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
